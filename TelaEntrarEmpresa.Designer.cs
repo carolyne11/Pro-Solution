7dfcc369ff17731e93fa2457d357ea4abe2a77f0
carolyne Araujo <xaviercarolyne36@gmail.com>
2025-05-07 21:09:59 -0300

    ATUAL 21:09

--- TelaEntrarEmpresa.Designer.cs
+++ TelaEntrarEmpresa.Designer.cs
@@ -29,9 +29,7 @@
         private void InitializeComponent()
         {
             pictureBox1 = new PictureBox();
-            textBox1 = new TextBox();
             textBox2 = new TextBox();
-            button1 = new Button();
             button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
@@ -46,38 +44,19 @@
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
-            // textBox1
-            // 
-            textBox1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(280, 113);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(258, 27);
-            textBox1.TabIndex = 1;
-            textBox1.Text = "GRÁFICO DE RESPOSTA POR ÁREA";
-            // 
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            textBox2.Location = new Point(317, 255);
+            textBox2.Location = new Point(308, 170);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(168, 27);
             textBox2.TabIndex = 2;
             textBox2.Text = " DADOS DE RESPOSTA";
             // 
-            // button1
-            // 
-            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(359, 156);
-            button1.Name = "button1";
-            button1.Size = new Size(96, 45);
-            button1.TabIndex = 3;
-            button1.Text = "ACESSAR";
-            button1.UseVisualStyleBackColor = true;
-            // 
             // button2
             // 
             button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(359, 325);
+            button2.Location = new Point(345, 230);
             button2.Name = "button2";
             button2.Size = new Size(96, 43);
             button2.TabIndex = 4;
@@ -91,9 +70,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 570);
             Controls.Add(button2);
-            Controls.Add(button1);
             Controls.Add(textBox2);
-            Controls.Add(textBox1);
             Controls.Add(pictureBox1);
             Name = "TelaEntrarEmpresa";
             Text = "TelaEntrarEmpresa";
@@ -105,9 +82,7 @@
         #endregion
 
         private PictureBox pictureBox1;
-        private TextBox textBox1;
         private TextBox textBox2;
-        private Button button1;
         private Button button2;
     }
 }
